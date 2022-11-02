69d3d794fa27afd5635cc6521c76e51354224314
Dalton Dick <daltondick2010@gmail.com>
2022-11-02 10:46:21 -0500

    Add powershell script

---
+++ run.ps1
@@ -0,0 +1,1 @@
+docker run -it --rm -v ${PWD}/credentials.yaml:/usr/src/app/credentials.yaml -v ${PWD}/client_secrets.json:/usr/src/app/client_secrets.json -v ${PWD}/config.yaml:/usr/src/app/config.yaml -v ${PWD}/youtube.db:/usr/src/app/youtube.db -v ${PWD}/logs:/usr/src/app/logs echidna
