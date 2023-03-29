a5886dadfea372e32ad07c05116bf9948725f12b
Dalton Dick <daltondick2010@gmail.com>
2023-03-29 17:51:52 -0500

    Fix script

--- scripts/push_to_s3.sh
+++ scripts/push_to_s3.sh
@@ -8,13 +8,9 @@ aws s3 cp credentials.yaml s3://$S3_BUCKET
 aws s3 cp .env s3://$S3_BUCKET
 
 if [ -f "$XDG_CONFIG_HOME/echidna/config.yaml" ]; then
-  aws s3 cp $XDG_CONFIG_HOME/echidna/config.yaml s3://$S3_BUCKET/config.yaml
-fi
-
-if [ -f "$HOME/.config/echidna/config.yaml" ]; then
-  aws s3 cp $HOME/.config/echidna/config.yaml s3://$S3_BUCKET/config.yaml
-fi
-
-if [ -f "./config.yaml" ]; then
+  aws s3 cp "$XDG_CONFIG_HOME/echidna/config.yaml" "s3://$S3_BUCKET/config.yaml"
+elif [ -f "$HOME/.config/echidna/config.yaml" ]; then
+  aws s3 cp "$HOME/.config/echidna/config.yaml" "s3://$S3_BUCKET/config.yaml"
+elif [ -f "./config.yaml" ]; then
   aws s3 cp ./config.yaml s3://$S3_BUCKET/config.yaml
 fi
