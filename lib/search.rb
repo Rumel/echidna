97dc6d8ddfee19274551b615048334f30f45c9cb
Dalton Dick <daltondick2010@gmail.com>
2024-08-21 18:39:50 -0500

    Search script

--- lib/search.rb
+++ lib/search.rb
@@ -3,6 +3,12 @@ require_relative './echidna/services/youtube'
 
 yt = Echidna::YoutubeService.new
 search_term = ARGV.join(' ')
+
+if search_term.empty?
+  puts 'Please provide a search term'
+  exit 0
+end
+
 puts "Searching for '#{search_term}'"
 
 result = yt.get_channel_id(search_term)
