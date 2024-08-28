c577f472806034e58af5652c7afadcaed985695c
Dalton Dick <daltondick2010@gmail.com>
2024-08-28 12:10:42 -0500

    Fix scripts

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -4,6 +4,6 @@ set -x
 source ~/Code/echidna/.env
 
 ~/Code/echidna/scripts/auth_ecr.sh
-~/Code/echidna/scriptes/pull_from_ecr.sh
+~/Code/echidna/scripts/pull_from_ecr.sh
 ~/Code/echidna/scripts/pull_from_s3.sh
 ~/Code/echidna/scripts/run.sh
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-docker run -it --rm \
-	-v ${PWD}/credentials.yaml:/usr/src/app/credentials.yaml \
-	-v ${PWD}/client_secrets.json:/usr/src/app/client_secrets.json \
-	-v ${PWD}/config.yaml:/usr/src/app/config.yaml \
-	-v ${PWD}/logs:/usr/src/app/logs \
-	-v ${PWD}/.env:/usr/src/app/.env \
-	echidna
-
+docker run --rm \
+  -v ${PWD}/credentials.yaml:/usr/src/app/credentials.yaml \
+  -v ${PWD}/client_secrets.json:/usr/src/app/client_secrets.json \
+  -v ${PWD}/config.yaml:/usr/src/app/config.yaml \
+  -v ${PWD}/logs:/usr/src/app/logs \
+  -v ${PWD}/.env:/usr/src/app/.env \
+  echidna
