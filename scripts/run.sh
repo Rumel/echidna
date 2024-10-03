43a43c4849df107cb09f27fd615c84d28528ad2a
Dalton Dick <daltondick2010@gmail.com>
2024-10-03 13:17:01 -0500

    Add back in --rm properly

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -rm \
+docker run --rm \
   -v ${PWD}/credentials.yaml:/usr/src/app/credentials.yaml \
   -v ${PWD}/client_secrets.json:/usr/src/app/client_secrets.json \
   -v ${PWD}/config.yaml:/usr/src/app/config.yaml \
